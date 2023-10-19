a6343e10e901ba72c32e08191c5e1891e6b3bf10
OlegShinka <volkonxxl@gmail.com>
2023-10-19 20:47:31 +0300

    app+form on hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,90 +1,62 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import { ContactsList } from './contactsList/contactsList';
 import Form from './form/form';
 import { Filter } from './filter/filter';
 
-export class App extends Component {
-  state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
-    filter: '',
-  };
+export const App = () => {
+  const [contacts, setContacts] = useState(
+    () => JSON.parse(localStorage.getItem('contacts') ?? '')
+    //виклик анонімної фу - при перезавантаж не буде очистки localeStorage
+  );
+  const [filter, setFilter] = useState('');
 
-  componentDidMount() {
-    const parseContacts = JSON.parse(localStorage.getItem('contacts'));
-    this.setState(prevState => ({
-      contacts: prevState.contacts,
-    }));
-    if (parseContacts) {
-      this.setState({ contacts: parseContacts });
-    }
-  }
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
 
-  componentDidUpdate(prevState) {
-    if (this.state.contacts !== prevState.contacts) {
-      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
-    }
-  }
-
-  checkContact = (nameContact, contacts) => {
+  const checkContact = (nameContact, contacts) => {
     return contacts.find(item => {
       return item.name.toLowerCase() === nameContact.toLowerCase();
     });
   };
 
-  formSubmitHandler = newContact => {
-    if (this.checkContact(newContact.name, this.state.contacts)) {
+  const formSubmitHandler = newContact => {
+    if (checkContact(newContact.name, contacts)) {
       alert(`${newContact.name} is already in contacts.`);
       return;
     }
-    this.setState(prev => ({
-      contacts: [...prev.contacts, newContact],
-    }));
+    setContacts(prev => [...prev, newContact]);
   };
-  onChangeFilter = value => {
-    this.setState({ filter: value });
+
+  const onChangeFilter = value => {
+    setFilter(value);
   };
 
   //фу видалення контакту по id
-  onDeleteContact = idContact => {
-    this.setState(prevState => ({
-      contacts: prevState.contacts.filter(item => item.id !== idContact),
-    }));
+  const onDeleteContact = idContact => {
+    // змінює стан contacts
+    setContacts(contacts.filter(item => item.id !== idContact));
   };
 
-  onContacts = filter => {
+  const onContacts = filter => {
     if (filter === '') {
-      return this.state.contacts;
+      return contacts;
     } else {
-      return this.state.contacts.filter(item => {
+      return contacts.filter(item => {
         return item.name.includes(filter.toLowerCase());
       });
     }
   };
-  //useState сніппет
-  render() {
-    const { filter } = this.state;
 
-    // const visibleContact = contacts.filter(contact =>
-    //   contact.name.includes(filter)
-    // );
-    return (
-      <div>
-        <h2> Phonebook</h2>
-        <Form onSubmitForm={this.formSubmitHandler} />
-        <h2>Contacts</h2>
-        <Filter filter={filter} onChangeFilter={this.onChangeFilter} />
-        <ContactsList
-          list={this.onContacts(filter)}
-          onDelete={this.onDeleteContact}
-        />
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      <h2> Phonebook</h2>
+      <Form onSubmitForm={formSubmitHandler} />
+      <h2>Contacts</h2>
+      <Filter filter={filter} onChangeFilter={onChangeFilter} />
+      <ContactsList list={onContacts(filter)} onDelete={onDeleteContact} />
+    </div>
+  );
+};
 
 export default App;
--- src/components/form/form.jsx
+++ src/components/form/form.jsx
@@ -1,68 +1,73 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { nanoid } from 'nanoid';
 import { Cont, Inp, FormStyle, BtnStyle } from './form.styled';
 import { TiUser, TiSortNumericallyOutline } from 'react-icons/ti';
-const INITIAL_STATE = { name: '', number: '' };
 
-class Form extends Component {
-  state = { ...INITIAL_STATE };
-  handleChange = evt => {
+export const Form = ({ onSubmitForm }) => {
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
+
+  const handleChange = evt => {
     const { name, value } = evt.target;
-    this.setState({
-      [name]: value,
-    });
+    switch (name) {
+      case 'name':
+        setName(value);
+        break;
+      case 'number':
+        setNumber(value);
+        break;
+      default:
+        return;
+    }
   };
 
-  handleSubmit = evt => {
+  const handleSubmit = evt => {
     evt.preventDefault();
-
+    //додаємо до обєкту властивості name, num та їх значення із масивів які повертає кожен useState
     const newContact = {
       id: nanoid(),
-      ...this.state,
+      name: name,
+      number: number,
     };
-    this.props.onSubmitForm(newContact); //виклик методу з App
-    this.resetForm();
+    onSubmitForm(newContact); //виклик методу з App
+    // чистим інпути після сабміту
+    setName('');
+    setNumber('');
   };
-  resetForm = () => {
-    //скидання інпутів
-    this.setState({
-      ...INITIAL_STATE,
-    });
-  };
-  render() {
-    return (
-      <div>
-        <Cont>
-          <FormStyle onSubmit={this.handleSubmit}>
-            <label>
-              Name <TiUser />
-            </label>
-            <Inp
-              type="text"
-              name="name"
-              value={this.state.name}
-              onChange={this.handleChange}
-              pattern="^[a-zA-Z][a-zA-Z0-9-_\.]{1,20}$"
-              required
-            />
-            <label htmlFor="tel">
-              Number <TiSortNumericallyOutline />
-            </label>
-            <Inp
-              type="tel"
-              value={this.state.number}
-              name="number"
-              onChange={this.handleChange}
-              // pattern="[a-zA-Z0-9]+"
-              pattern="\\+?\\d{1,4}?[ .\\-\\s]?\\(?\\d{1,3}?\\)?[ .\\-\\s]?\\d{1,4}[ .\\-\\s]?\\d{1,4}[ .\\-\\s]?\\d{1,9}"
-              required
-            />
-            <BtnStyle type="submit">Add contact</BtnStyle>
-          </FormStyle>
-        </Cont>
-      </div>
-    );
-  }
-}
+
+  return (
+    <div>
+      <Cont>
+        <FormStyle onSubmit={handleSubmit}>
+          <label>
+            Name <TiUser />
+          </label>
+          <Inp
+            autoComplete="on"
+            type="text"
+            name="name"
+            value={name}
+            onChange={handleChange}
+            pattern="^[a-zA-Z][a-zA-Z0-9-_\.]{1,20}$"
+            required
+          />
+          <label htmlFor="tel">
+            Number <TiSortNumericallyOutline />
+          </label>
+          <Inp
+            type="tel"
+            value={number}
+            name="number"
+            onChange={handleChange}
+            // pattern="[a-zA-Z0-9]+"
+            pattern="\\+?\\d{1,4}?[ .\\-\\s]?\\(?\\d{1,3}?\\)?[ .\\-\\s]?\\d{1,4}[ .\\-\\s]?\\d{1,4}[ .\\-\\s]?\\d{1,9}"
+            required
+          />
+          <BtnStyle type="submit">Add contact</BtnStyle>
+        </FormStyle>
+      </Cont>
+    </div>
+  );
+};
 
 export default Form;
